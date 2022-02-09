4dd023acf86157fdeb5fdf254fad9eec02de2e88
Breno Maia <50585842+BrenosmMaia@users.noreply.github.com>
2022-02-09 19:21:56 -0300

    Update Hello2DApp.java

--- 02-Java2D-Hello/Hello2DApp.java
+++ 02-Java2D-Hello/Hello2DApp.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.Ellipse2D;
 import javax.swing.*;
 
 public class Hello2DApp {
@@ -11,11 +12,11 @@ public class Hello2DApp {
 class Hello2DFrame extends JFrame {
     public Hello2DFrame () {
         this.addWindowListener (
-            new WindowAdapter() {
-                public void windowClosing (WindowEvent e) {
-                    System.exit(0);
+                new WindowAdapter() {
+                    public void windowClosing (WindowEvent e) {
+                        System.exit(0);
+                    }
                 }
-            }
         );
         this.setTitle("Java2D - Hello World!");
         this.setSize(350, 350);
@@ -25,10 +26,17 @@ class Hello2DFrame extends JFrame {
     public void paint (Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setPaint(Color.blue);
         int w = getWidth();
         int h = getHeight();
+        g2d.setPaint(Color.black);
+        g2d.fillRect(0,0,w,h);
+        g2d.setPaint(Color.red);
         g2d.drawLine(0,0, w,h);
         g2d.drawLine(0,h, w,0);
+        g2d.drawLine(w/2,0, w/2,h);
+        g2d.drawLine(0,h/2, w,h/2);
+        g2d.draw(new Ellipse2D.Float(w/3, h/3,
+                w/3,
+                h/3));
     }
 }
