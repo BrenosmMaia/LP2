946074db0f384679ae3e5a69eed65184631a3a41
Breno Maia <50585842+BrenosmMaia@users.noreply.github.com>
2022-02-16 20:20:19 -0300

    Update Elipse.c

--- 2.1.1/Elipse.c
+++ 2.1.1/Elipse.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 typedef struct {
-  int f1, f2;
+  int f1x, f1y;
+  int f2x, f2y;
   int eixo;
 } Elipse;
 
 void print (Elipse* e) {
-  printf("Elipse com focos (%d, %d) e eixo maior %d.\n", e->f1,e->f2, e->eixo);
+  printf("Elipse com focos (%d, %d) e (%d, %d) e eixo maior %d.\n", e->f1x, e->f1y, e->f2x, e->f2y, e->eixo);
 }
