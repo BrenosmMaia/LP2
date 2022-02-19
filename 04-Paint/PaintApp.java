5c02db03560d774fd613dd6f3e4d4b2eaff5f38a
Breno Maia <50585842+BrenosmMaia@users.noreply.github.com>
2022-02-19 20:17:04 -0300

    Update PaintApp.java

--- 04-Paint/PaintApp.java
+++ 04-Paint/PaintApp.java
@@ -10,45 +10,57 @@ class PaintApp {
 }
 
 class PaintFrame extends JFrame {
-    Rect r1;
+    Rect r1, r2, r3, r4;
 
     PaintFrame () {
         this.addWindowListener (
-            new WindowAdapter() {
-                public void windowClosing (WindowEvent e) {
-                    System.exit(0);
+                new WindowAdapter() {
+                    public void windowClosing (WindowEvent e) {
+                        System.exit(0);
+                    }
                 }
-            }
         );
         this.setTitle("Painting Figures");
         this.setSize(350, 350);
-        this.r1 = new Rect(50,50, 100,30);
+        this.r1 = new Rect(50,50, 100,30, Color.red, Color.black);
+        this.r2 = new Rect(50,80, 100,30, Color.blue, Color.black);
+        this.r3 = new Rect(50,110, 100,30, Color.yellow, Color.black);
+        this.r4 = new Rect(50,140, 100,30, Color.green, Color.black);
     }
 
     public void paint (Graphics g) {
         super.paint(g);
         this.r1.paint(g);
+        this.r2.paint(g);
+        this.r3.paint(g);
+        this.r4.paint(g);
     }
 }
 
 class Rect {
     int x, y;
     int w, h;
+    Color fill, cont;
 
-    Rect (int x, int y, int w, int h) {
+    Rect (int x, int y, int w, int h, Color fill, Color cont) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.fill = fill;
+        this.cont = cont;
     }
 
     void print () {
         System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
-            this.w, this.h, this.x, this.y);
+                this.w, this.h, this.x, this.y);
     }
 
     void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawRect(this.x,this.y, this.w,this.h);
+        g2d.setColor(this.fill);
+        g2d.fillRect(this.x,this.y,this.w,this.h);
+        g2d.setColor(this.cont);
+        g2d.drawRect(this.x, this.y, this.w, this.h);
     }
 }
