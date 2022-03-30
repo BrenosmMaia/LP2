e727d76846dd2f91e6c591e882f74120ee2805d5
Breno Maia <50585842+BrenosmMaia@users.noreply.github.com>
2022-03-30 20:29:05 -0300

    Update Figure.java

--- Projetor Editor/figures/Figure.java
+++ Projetor Editor/figures/Figure.java
@@ -3,9 +3,9 @@ package figures;
 import java.awt.*;
 
 public abstract class Figure {
-    int x, y;
-    int w, h;
-    Color fill;
+    protected int x, y;
+    protected int w, h;
+    protected Color fill;
 
     public Figure(int x, int y, int w, int h, Color fill) {
         this.x = x;
@@ -16,4 +16,22 @@ public abstract class Figure {
     }
 
     public abstract void paint (Graphics g);
+
+    public void drag(int delta_x, int delta_y) {
+        this.x += delta_x;
+        this.y += delta_y;
+    }
+
+    public void reSize(int s) {
+        this.w += s;
+        this.h +=s;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
 }
