9d6f4eeaa44bc178cbaf3c0c69abb804471c750e
Breno Maia <50585842+BrenosmMaia@users.noreply.github.com>
2022-04-01 17:00:49 -0300

    Update Ellipse.java

--- Projetor Editor/figures/Ellipse.java
+++ Projetor Editor/figures/Ellipse.java
@@ -5,18 +5,26 @@ import java.awt.geom.Ellipse2D;
 import java.awt.geom.Ellipse2D.Double;
 
 public class Ellipse extends Figure {
-    private Color cont;
+    private Color fill;
+    private Ellipse2D Ellipse;
 
     public Ellipse (int x, int y, int w, int h, Color fill, Color cont) {
-        super(x, y, w, h, fill);
-        this.cont = cont;
+        super(x, y, w, h, cont);
+        this.fill = fill;
     }
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
+        this.Ellipse = new Ellipse2D.Double(this.x, this.y, this.w, this.h);
         g2d.setColor(this.fill);
-        g2d.fill (new Ellipse2D.Double(this.x, this.y, this.w, this.h));
+        g2d.fill(this.Ellipse);
         g2d.setColor(this.cont);
-        g2d.draw(new Ellipse2D.Double(this.x, this.y, this.w, this.h));
+        g2d.draw(this.Ellipse);
+    }
+
+    public boolean isClicked(int x, int y) {
+        if(this.Ellipse != null)
+            return this.Ellipse.contains(x, y);
+        return false;
     }
 }
