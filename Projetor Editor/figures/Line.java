5deb74fc708146b865d15be29fccbf83d43bca0c
Breno Maia <50585842+BrenosmMaia@users.noreply.github.com>
2022-04-01 17:01:17 -0300

    Update Line.java

--- Projetor Editor/figures/Line.java
+++ Projetor Editor/figures/Line.java
@@ -1,16 +1,25 @@
 package figures;
 
 import java.awt.*;
+import java.awt.geom.Line2D;
 
 public class Line extends Figure {
+    private Line2D Line;
 
-    public Line (int x, int y, int w, int h, Color fill) {
-        super(x, y, w, h, fill);
+    public Line (int x, int y, int w, int h, Color cont) {
+        super(x, y, w, h, cont);
     }
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(this.fill);
-        g2d.drawLine(this.x,this.y, this.w, this.h);
+        Line = new Line2D.Double(this.x,this.y, this.w, this.h);
+        g2d.setColor(this.cont);
+        g2d.draw(Line);
+    }
+
+    public boolean isClicked(int x, int y) {
+        if(this.Line != null)
+            return this.Line.contains(x, y);
+        return  false;
     }
 }
