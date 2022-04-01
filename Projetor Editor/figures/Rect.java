dc0679524029a9b58e293063bb2774a4740817ca
Breno Maia <50585842+BrenosmMaia@users.noreply.github.com>
2022-04-01 17:00:28 -0300

    Update Rect.java

--- Projetor Editor/figures/Rect.java
+++ Projetor Editor/figures/Rect.java
@@ -1,20 +1,29 @@
 package figures;
 
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 public class Rect extends Figure {
-    private Color cont;
+    private Color fill;
+    private Rectangle2D Rect;
 
     public Rect (int x, int y, int w, int h, Color fill, Color cont) {
-        super(x, y, w, h, fill);
-        this.cont = cont;
+        super(x, y, w, h, cont);
+        this.fill = fill;
     }
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
+        this.Rect = new Rectangle2D.Double(this.x, this.y, this.w, this.h);
         g2d.setColor(this.fill);
-        g2d.fillRect(this.x,this.y,this.w,this.h);
+        g2d.fill(this.Rect);
         g2d.setColor(this.cont);
-        g2d.drawRect(this.x, this.y, this.w, this.h);
+        g2d.draw(this.Rect);
+    }
+
+    public boolean isClicked(int x, int y) {
+        if(this.Rect != null)
+            return this.Rect.contains(x, y);
+        return false;
     }
 }
