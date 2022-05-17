772b9559468fad3b346288418ad8d6255cc425b3
Breno Maia <50585842+BrenosmMaia@users.noreply.github.com>
2022-05-16 21:49:00 -0300

    Update Triangle.java

--- Projetor Editor/figures/Triangle.java
+++ Projetor Editor/figures/Triangle.java
@@ -11,14 +11,17 @@ public class Triangle extends Figure {
         this.fill = fill;
     }
 
-    public void paint(Graphics g) {
+    public void paint(Graphics g, boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
         int[] xValues = { this.x, this.x+(this.w/2), this.x+this.w};
         int[] yValues = { this.y+this.h, this.y, this.y + this.h,};
         this.Triangle = new Polygon( xValues, yValues, 3);
         g2d.setColor(this.fill);
         g2d.fill(this.Triangle);
-        g2d.setColor(this.cont);
+        if (focused)
+            g2d.setColor(Color.red);
+        else
+            g2d.setColor(this.cont);
         g2d.draw(this.Triangle);
     }
 
