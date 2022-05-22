5fe22234ef542037327382535b11aa9576df8fce
Breno Maia <50585842+BrenosmMaia@users.noreply.github.com>
2022-05-22 20:39:40 -0300

    Update Ellipse.java

--- Projetor Editor/figures/Ellipse.java
+++ Projetor Editor/figures/Ellipse.java
@@ -3,14 +3,14 @@ package figures;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Ellipse2D.Double;
+import java.awt.geom.Rectangle2D;
 
 public class Ellipse extends Figure {
-    private Color fill;
     private Ellipse2D Ellipse;
+    private Ellipse2D foco;
 
     public Ellipse (int x, int y, int w, int h, Color fill, Color cont) {
-        super(x, y, w, h, cont);
-        this.fill = fill;
+        super(x, y, w, h, fill, cont);
     }
 
     public void paint (Graphics g, boolean focused) {
@@ -18,11 +18,14 @@ public class Ellipse extends Figure {
         this.Ellipse = new Ellipse2D.Double(this.x, this.y, this.w, this.h);
         g2d.setColor(this.fill);
         g2d.fill(this.Ellipse);
-        if (focused)
-            g2d.setColor(Color.red);
-        else
-            g2d.setColor(this.cont);
+        g2d.setColor(this.cont);
         g2d.draw(this.Ellipse);
+        if (focused) {
+            Graphics2D focusline = (Graphics2D) g;
+            this.foco = new Ellipse2D.Double(this.x-3,this.y-3,this.w+6,this.h+6);
+            focusline.setColor(Color.red);
+            focusline.draw(this.foco);
+        }
     }
 
     public boolean isClicked(int x, int y) {
@@ -36,7 +39,20 @@ public class Ellipse extends Figure {
             this.fill = Color.blue;
         else if(this.fill == Color.blue)
             this.fill = Color.green;
+        else if (this.fill == Color.green)
+            this.fill = Color.cyan;
         else
             this.fill = Color.black;
+    }
+
+    public void changeCont() {
+        if (this.cont == Color.black)
+            this.cont = Color.blue;
+        else if (this.cont == Color.blue)
+            this.cont = Color.green;
+        else if (this.cont == Color.green)
+            this.cont = Color.cyan;
+        else
+            this.cont = Color.black;
     }
 }
